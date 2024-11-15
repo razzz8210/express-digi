e3037132919a254ab4a09b8a7575a8b58d799826
Raj Jaiswal <razzz8210@gmail.com>
2024-11-15 12:35:09 +0530

    adding env files

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+import 'dotenv/config.js'
+//require('dotenv').config()
+
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000
 app.use(express.json())
 
 let teaData = []
